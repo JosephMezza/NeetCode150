6fbb12c934f2b399c826f0b0bdb65a61dde0c5d5
JosephMezza <60296506+JosephMezza@users.noreply.github.com>
2025-02-02 22:18:54 -0500

    optimal solution

--- longest-consecutive-sequence/Solution.java
+++ longest-consecutive-sequence/Solution.java
@@ -1,49 +1,84 @@
 
-import java.util.Arrays;
+import java.util.HashSet;
+
+//Time complexity: This runs in O(n) time because we only search for consecutive numbers if it is
+//the FIRST in the sequence, this is done by checking is the number-1 does not exist in the HashSet.
+//Space complexity: This is O(n) because we need to keep a HashSet of all the numbers.
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        int max = 0;
+
+        for(int num : set){//for some reason, itterating over the HashSet does NOT cause a timeout, inlike the array... IDK WHY.
+            if(!set.contains(num-1)){
+                int currentCount = 1;
+                int currentNum = num;
+
+                while(set.contains(currentNum+1)){
+                    currentNum++;
+                    currentCount++;
+                }
+
+                max = Math.max(currentCount, max);
+            }
+        }
+        return max;
+    }
+}
 
 //Time complexity: It is O(n*log(n)) where n is the size of the array. The most costly operation
 //is sorting the array which is n*log(n) and then we itterate over it linearly so n. O(n*log(n) + n) -> O(n*log(n)) 
 //Space complexity: It is O(1) since we porform the sort on the starting array and we use the max and localMax
 //ints to track the maximum consecutive streak.
-class Solution {
-    public int longestConsecutive(int[] nums) {
-        //[]
-        if(nums == null || nums.length == 0){
-            return 0;
-        }
-        //[1]
-        if(nums.length == 1){
-            return 1;
-        }
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         //[]
+//         if(nums == null || nums.length == 0){
+//             return 0;
+//         }
+//         //[1]
+//         if(nums.length == 1){
+//             return 1;
+//         }
 
-        Arrays.sort(nums);
+//         Arrays.sort(nums);
 
-        //1,2,3
-        //1,3,5
-        //2,3,4,4,5,10,20
-        //1,3,4,6,7,8,20
-        int max = 0;
-        for(int i=0; i < nums.length-1;i++){
-            int localMax = 1;
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[j] == nums[j-1]){
-                    continue;
-                }
+//         //1,2,3
+//         //1,3,5
+//         //2,3,4,4,5,10,20
+//         //1,3,4,6,7,8,20
+//         int max = 0;
+//         for(int i=0; i < nums.length-1;i++){
+//             int localMax = 1;
+//             for(int j=i+1; j<nums.length; j++){
+//                 if(nums[j] == nums[j-1]){
+//                     continue;
+//                 }
 
-                if(nums[i] + localMax != nums[j]){
-                    i = j-1;
-                    break;
-                }
+//                 if(nums[i] + localMax != nums[j]){
+//                     i = j-1;
+//                     break;
+//                 }
 
-                localMax++;
-            }
+//                 localMax++;
+//             }
 
-            max = Math.max(localMax, max);
-        }
+//             max = Math.max(localMax, max);
+//         }
 
-        return max;
-    }
-}
+//         return max;
+//     }
+// }
 
 //Time complexity: It is O(n^2) since we need to itterate over the array once again for each element to see
 //if it can make a sequence.
