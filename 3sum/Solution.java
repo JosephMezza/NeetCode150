b6c941d74b058ab1b28fc35f624cc7c8ed0dcea2
JosephMezza <60296506+JosephMezza@users.noreply.github.com>
2025-02-05 20:08:02 -0500

    Fixed solution (still inefficient)

--- 3sum/Solution.java
+++ 3sum/Solution.java
@@ -1,7 +1,7 @@
 //Time complexity: This is (1) O(n^3) *  (2) O(n^3). (1) is because we need to check every
 //possible triple which is n^3, where n is the size of the array. We need to multiply this
 //because we need to check for unique triples, so in the worst case, we need to itterate over
-//a list that could have every triple in it at size n^3. So n^3*n^3 = O(n^6).
+//a list that could have every triple in it at size n^3. We also do a sort which is n*log(n). So n^3*(n^3+(n*log(n))) = O(n^6).
 //VERY INNEFICCIENT. Thinking of a better solution.
 //Time complexity: This is O(1) since our list is required by the function and we use
 //the same one to check for duplicated so no extra memory.
@@ -14,9 +14,11 @@ class Solution {
             for(int j=i+1; j<nums.length; j++){
                 for(int k=j+1; k<nums.length; k++){
                     if(nums[i]+nums[j]+nums[k] == 0){
+                        List<Integer> t = new ArrayList<>(List.of(nums[i], nums[j], nums[k]));
+                        Collections.sort(t);
                         boolean alreadyExists = false;
                         for(List<Integer> l : triples){
-                            if(l.contains(nums[i]) && l.contains(nums[j]) && l.contains(nums[k])){
+                            if(l.equals(t)){
                                 alreadyExists = true;
                                 break;
                             }
@@ -25,7 +27,7 @@ class Solution {
                             continue;
                         }
 
-                        triples.add(new ArrayList<Integer>(List.of(nums[i],nums[j],nums[k])));
+                        triples.add(t);
                     }
                 }
             }
